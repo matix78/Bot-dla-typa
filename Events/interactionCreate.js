21c051eeef6f97d6359892ecbb9bf1bf28c051e7
PAW122 <70094237+PAW122@users.noreply.github.com>
2022-07-31 01:58:18 +0200

    reapir interacyionCreate

--- Events/interactionCreate.js
+++ Events/interactionCreate.js
@@ -1,24 +1,22 @@
 const Discord = require('discord.js');
 const { Permissions: { FLAGS } } = require('discord.js');
-
+let lastInteractionId;
 module.exports = {
-    name: 'interactionCreate',
-
-    async execute(inter, client) {
+    name: 'interactionCreate', async execute(inter, client) {
+        if(lastInteractionId && lastInteractionId == inter.id) return;
         if (!inter.isCommand()) return;
-
         const command = client.commands.get(inter.commandName);
-
         if (!command) return;
-
+        lastInteractionId = inter.id
         try {
-            await command.execute(inter);
+            await command.executeInteraction(inter);
         } catch (error) {
             console.error(error);
-            await inter.reply({
-                content: 'Wystąpił błąd w komendzie!',
-                ephemeral: true
-            });
+            
+            await inter.reply({ content: 'Wystąpił błąd w komendzie!', ephemeral: true });
+
         }
     }
 }
+
+
--- functions/handlerevents.js
+++ functions/handlerevents.js
@@ -3,8 +3,10 @@ module.exports = (client) => {
         for (const file of eventFiles) {
             const event = require(`../Events/${file}`);
             if(event.once) {
+                console.log(`wywołana komenda:${file}`)
                 client.once(event.name, (...args) => event.execute(...args, client));
             }else {
+                console.log(`wywołana komenda2:${file}`)
                 client.on(event.name, (...args) => event.execute(...args, client));
             }
         }
