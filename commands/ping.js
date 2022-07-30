716e8cbcab74fef347a231d0df4eabf979af8d40
PAW122 <70094237+PAW122@users.noreply.github.com>
2022-07-31 01:23:26 +0200

    add ping.js

---
+++ commands/ping.js
@@ -0,0 +1,24 @@
+
+const { SlashCommandBuilder } = require('@discordjs/builders');
+const Discord = require('discord.js');
+
+module.exports = {
+
+    data: new SlashCommandBuilder()
+        .setName('ping')
+        .setDescription('Wyświetla aktualny ping bota'),
+    executeInteraction: async (inter) => {
+        
+        const embed = new Discord.MessageEmbed()
+            .setTitle('**PONG**')
+            .setColor('RANDOM')
+            .setDescription(`${Date.now() - inter.createdTimestamp}ms \nms. API Latency is ${Math.round(inter.client.ws.ping)}ms`)
+        inter.reply({ embeds: [embed] });
+    },
+
+
+    execute: async (message, args) => {
+
+    }
+}
+
