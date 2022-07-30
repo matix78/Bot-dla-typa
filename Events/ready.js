d7a020f853b4d7411ac8c50a8e3d74f75f61e31b
matix78 <79283694+matix78@users.noreply.github.com>
2022-07-31 01:42:45 +0200

    Edit ready.js

--- Events/ready.js
+++ Events/ready.js
@@ -9,16 +9,6 @@ module.exports = {
 
     async execute(client) {
         console.log('Bot jest online');
-        const statuses = [
-            { name: 'Spotify', type: 'LISTENING' },
-            { name: 'CoinLabiryntCollect', type: 'PLAYING' },
-            { name: `${client.guilds.cache.size} serwery`, type: 'WATCHING' },
-            { name: 'komendy', type: 'WATCHING'},
-        ];
-        setInterval(() => {
-            var randomStatus = statuses[Math.floor(Math.random() * statuses.length)];
-            client.user.setActivity(randomStatus);
-        }, 10000);
         const rest = new REST({ version: '9' }).setToken(token);
         (async () => {
             try {
